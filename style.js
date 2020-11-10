fe3e1a72d17cbe737279f72a1498144d2cd90baf
Jules <juliannewood2413@gmail.com>
2020-11-10 14:53:43 -0700

    initial commit for updated portfolio

---
+++ style.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+
+//Portfolio Page Links
+$("#repo1").click(function(){
+    window.location.href = "https://juliannewood2413.github.io/weather-dashboard-assignment/"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// })
