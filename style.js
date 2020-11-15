8a51236d7bfcb53ac17ede2184dc481df3daadc4
Jules <juliannewood2413@gmail.com>
2020-11-15 10:43:05 -0700

    updates to contact page

--- style.js
+++ style.js
@@ -2,17 +2,39 @@ $(document).ready(function() {
 
 //Portfolio Page Links
 $("#repo1").click(function(){
-    window.location.href = "https://juliannewood2413.github.io/weather-dashboard-assignment/"
+    window.open("https://juliannewood2413.github.io/weather-dashboard-assignment/")
 });
 
 $("#repo2").click(function(){
-    window.location.href = "https://juliannewood2413.github.io/HW-day-scheduler/"
+    window.open("https://juliannewood2413.github.io/HW-day-scheduler/")
 });
 
 $("#repo3").click(function(){
-    window.location.href = "https://juliannewood2413.github.io/Lets_get_trivial/"
+    window.open("https://juliannewood2413.github.io/Lets_get_trivial/")
 })
 
+$("#gitIcon").click(function(){
+    window.open("https://github.com/Juliannewood2413")
+})
+
+$("#linkedIcon").click(function(){
+    window.open("https://www.linkedin.com/in/julianne-wood-1392261b5?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bk12rQLfXQVuyMAs1Aq70mw%3D%3D")
+})
+
+$("#resumeBtn").click(function(){
+    window.open("https://juliannewood2413.github.io/Resume_file/")
+})
+
+
+//Contact Page links
+
+$("#eMail").click(function (){
+   $("#eMail").attr('href', 'mailto:juliannewood2413@gmail.com')
+});
+
+$("#linkedIcon2").click(function(){
+    window.open("https://www.linkedin.com/in/julianne-wood-1392261b5?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bk12rQLfXQVuyMAs1Aq70mw%3D%3D")
+})
 
 
 
